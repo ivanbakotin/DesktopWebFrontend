168e68cdc7388b0a6fb8688a4ed05e49bba00c97
ivan <ivan.bakotin1999@gmail.com>
2022-03-07 16:49:35 +0100

    right click menu

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-for (let i = 0; i < 1e4; i++) {
-  console.log(i);
-}
+const body = document.querySelector("body");
+const rightClickMenu = document.querySelector(".right-click-menu")
+
+body.addEventListener("contextmenu", e => {
+  e.preventDefault();
+  console.log(e.clientX, e.clientY)
+  rightClickMenu.style.top = e.pageY - window.scrollY + "px";
+  rightClickMenu.style.left = e.pageX - window.scrollX + "px";
+  rightClickMenu.style.display = "block"
+})
+
+body.addEventListener("click", () => {
+  rightClickMenu.style.display = "none"
+})
